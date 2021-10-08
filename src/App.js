0a5c822033eea3bace846293a702ee24b61818db
Sukrutha <sukruthasathya@gmail.com>
2021-10-08 18:17:35 +0530

    Banner and navbar created

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import Navbar from './components/NavBar/Navbar';
+import "./App.css"
+import Banner from './components/Banner/Banner';
+
+
+function App() {
+ 
+  return (
+    <div className="App">
+      <Navbar/>
+      <Banner/>
+    </div>
+  )
+}
+
+export default App;
+
