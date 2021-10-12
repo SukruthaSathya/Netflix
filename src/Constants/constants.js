bea8cbb4f6142ff309a32c176563e5a630165edc
Sukrutha <sukruthasathya@gmail.com>
2021-10-13 00:12:00 +0530

    Axios & tmdb, banner movie updated

---
+++ src/Constants/constants.js
@@ -0,0 +1,3 @@
+export const baseUrl='https://api.themoviedb.org/3'
+export const API_KEY_HERE= '82a8c4f6581a3f61eb16672ac47da8bd'
+export const imageUrl='https://image.tmdb.org/t/p/original'
--- src/components/Banner/Banner.js
+++ src/components/Banner/Banner.js
@@ -1,16 +1,28 @@
-import React from 'react'
+import React,{ useEffect, useState } from 'react'
+import {API_KEY_HERE,imageUrl} from '../../Constants/constants'
 import './Banner.css'
+import axios from '../../axios'
 
 function Banner() {
+    const [movie,setMovie]=useState([])
+    useEffect(()=>{
+        axios.get(`/trending/all/week?api_key=${API_KEY_HERE}&language=en-US`).then((response)=>{
+            console.log(response.data.results[2]);
+            setMovie(response.data.results[2])
+        })
+    },[])
+   
     return (
-        <div className="banner">
+        <div 
+        style={{backgroundImage: `url(${movie ? imageUrl+movie.backdrop_path : ""})`}}
+        className="banner">
             <div className="content">
-                <h1 className="title">Movie Name</h1>
+                <h1 className="title">{movie.title}</h1>
                 <div className="banner_buttons">
                     <button className="button">Play</button>
                     <button className="button">My list</button>
                 </div>
-                <h1 className="description">In publishing and graphic design, Lorem ipsum is a placeholder text commonly used to demonstrate the visual form of a document or a typeface without relying on meaningful content.</h1>
+                <h1 className="description">{movie.overview}</h1>
             </div>
             <div className="fade_bottom"></div>
             
